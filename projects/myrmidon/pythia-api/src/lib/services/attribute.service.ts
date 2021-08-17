74a4215e11426e1f7bc10e30b8776d0515a35226
Myrmex <dfusi@hotmail.com>
2021-08-17 20:36:14 +0200

    Attribute service

---
+++ projects/myrmidon/pythia-api/src/lib/services/attribute.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EnvService, ErrorService } from '@myrmidon/pythia-core';
+import { Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export interface AttributeFilter {
+  pageNumber: number;
+  pageSize: number;
+  type: AttributeFilterType;
+  name?: string;
+}
+
+export enum AttributeFilterType {
+  Document = 0,
+  Structure,
+  Occurrence,
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AttributeService {
+  constructor(
+    private _http: HttpClient,
+    private _error: ErrorService,
+    private _env: EnvService
+  ) {}
+
+  /**
+   * Get the specified page of attribute names.
+   * @param filter The attributes filter. Set page size to 0 to get a
+   * single page with all the attribute names at once.
+   * @returns Page of names.
+   */
+  public getAttributeNames(filter: AttributeFilter): Observable<string[]> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.set('pageNumber', filter.pageNumber.toString());
+    httpParams = httpParams.set('pageSize', filter.pageSize.toString());
+    httpParams = httpParams.set('type', filter.type.toString());
+
+    if (filter.name) {
+      httpParams = httpParams.set('name', filter.name);
+    }
+
+    return this._http
+      .get<string[]>(this._env.get('apiUrl') + '/attributes', {
+        params: httpParams,
+      })
+      .pipe(retry(3), catchError(this._error.handleError));
+  }
+}
--- projects/myrmidon/pythia-api/src/public-api.ts
+++ projects/myrmidon/pythia-api/src/public-api.ts
@@ -6,4 +6,6 @@ export * from './lib/services/admin-guard.service';
 export * from './lib/services/auth-guard.service';
 export * from './lib/services/auth.interceptor';
 export * from './lib/services/auth.service';
+export * from './lib/services/attribute.service';
+export * from './lib/services/document.service';
 export * from './lib/pythia-api.module';
