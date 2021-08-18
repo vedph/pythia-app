98ed0d078a3d9baa0066688b83b43f164d195745
Myrmex <dfusi@hotmail.com>
2021-08-18 10:19:14 +0200

    Adding document lib

---
+++ projects/myrmidon/pythia-document/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of pythia-document
+ */
+
+export * from './lib/pythia-document.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { PythiaMaterialModule } from 'projects/myrmidon/pythia-material/src/public-api';
 import { PythiaCoreModule } from 'projects/myrmidon/pythia-core/src/public-api';
 import { PythiaApiModule } from 'projects/myrmidon/pythia-api/src/public-api';
+import { EnvServiceProvider } from '@myrmidon/pythia-core';
+import { AuthInterceptor } from '@myrmidon/pythia-api';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -20,12 +23,23 @@ import { PythiaApiModule } from 'projects/myrmidon/pythia-api/src/public-api';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    // Akita
+    AkitaNgDevtools.forRoot(),
     // Pythia
     PythiaCoreModule,
     PythiaMaterialModule,
-    PythiaApiModule
+    PythiaApiModule,
   ],
-  providers: [],
+  providers: [
+    EnvServiceProvider,
+    // HTTP interceptor
+    // https://medium.com/@ryanchenkie_40935/angular-authentication-using-the-http-client-and-http-interceptors-2f9d1540eb8
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
