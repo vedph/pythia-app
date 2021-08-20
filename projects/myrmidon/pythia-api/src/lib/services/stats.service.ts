5ffef9c0b9cf71d2a1c470b0c28bb54fd0064a6d
Myrmex <dfusi@hotmail.com>
2021-08-20 21:10:53 +0200

    Added stats service

---
+++ projects/myrmidon/pythia-api/src/lib/services/stats.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ErrorService, EnvService } from '@myrmidon/pythia-core';
+import { Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StatsService {
+  constructor(
+    private _http: HttpClient,
+    private _error: ErrorService,
+    private _env: EnvService
+  ) {}
+
+  /**
+   * Get statistics about the index.
+   * @returns Dictionary object with name=number pairs.
+   */
+  public getStatistics(): Observable<{ [key: string]: number }> {
+    return this._http
+      .get<{ [key: string]: number }>(this._env.get('apiUrl') + 'stats')
+      .pipe(retry(3), catchError(this._error.handleError));
+  }
+}
