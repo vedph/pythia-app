e3c11fbda3a6d9cafd5e224b4101b6bb4bed40b1
Myrmex <dfusi@hotmail.com>
2022-04-18 10:13:11 +0200

    Upgraded Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthJwtService, GravatarService, User } from '@myrmidon/auth-jwt-login';
+import { EnvService } from '@myrmidon/ng-tools';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 
@@ -11,11 +12,14 @@ import { take } from 'rxjs/operators';
 })
 export class AppComponent {
   public user$: Observable<User | null>;
+  public version: string;
 
   constructor(private _router: Router,
     private _authService: AuthJwtService,
-    private _gravatarService: GravatarService) {
+    private _gravatarService: GravatarService,
+    env: EnvService) {
     this.user$ = _authService.currentUser$;
+    this.version = env.get('version') || '';
   }
 
   public getGravatarUrl(email: string, size = 80): string | null {
